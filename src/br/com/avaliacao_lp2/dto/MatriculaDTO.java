1299b01b3030a2ef06654742362a8894508a3925
Dimas F <dimaslimaferreira@gmail.com>
2024-02-07 19:40:00 -0300

    first commit

---
+++ src/br/com/avaliacao_lp2/dto/MatriculaDTO.java
@@ -0,0 +1,14 @@
+package br.com.avaliacao_lp2.dto;
+
+
+public class MatriculaDTO {
+    private int idMatricula;
+
+    public int getIdMatricula() {
+        return idMatricula;
+    }
+
+    public void setIdMatricula(int idMatricula) {
+        this.idMatricula = idMatricula;
+    }
+}
